02f9f2636164415cfa8eb72f83944481df5167c8
Bhuvana-chandr <141762045+Bhuvana-chandr@users.noreply.github.com>
2023-10-27 10:58:25 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.*;
+import java.lang.*;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) throws Exception {
+		String driver ="com.mysql.jdbc.Driver";
+		String url ="jdbc:mysql://localhost:3306/Sietk";
+		String username ="root";
+		String password ="7569*@Bullet";
+		Class.forName(driver);
+
+		Connection con =DriverManager.getConnection(url,username,password);
+		Statement stmt =con.createStatement();
+
+		//stmt.executeUpdate("insert into students values(1,'b')");
+
+		ResultSet rs = stmt.executeQuery("select * from students");
+
+		while(rs.next())
+		{
+			System.out.println(rs.getInt("id")+rs.getString("name"));
+		}
+
+
+		con.close();
+	}
+
+}
